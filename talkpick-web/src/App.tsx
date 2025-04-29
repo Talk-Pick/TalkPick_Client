964f87dc5b8bfe55d6b65cecb82f2e1b75d79e95
kwangyoulsagong <sgky0511@naver.com>
2025-04-29 11:34:57 +0900

    feat: 제목 컴포넌트 추가

--- talkpick-web/src/App.tsx
+++ talkpick-web/src/App.tsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import "./Shared/font/font.css";
+import "./Shared/DesignSystem/font/font.css";
+import { Heading } from "./Shared/DesignSystem/Heading/Heading";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Heading variant="heading1">제목 22 ExtraBold </Heading>
+      <Heading variant="heading2">부제목 17 SemiBold </Heading>
+      <Heading variant="heading3">소제목 15 SemiBold</Heading>
     </>
   );
 }
