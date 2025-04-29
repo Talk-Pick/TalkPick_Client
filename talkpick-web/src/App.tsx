81b2e8e937be3e77444e1ef8ae646d9aa180d744
kwangyoulsagong <sgky0511@naver.com>
2025-04-29 11:39:39 +0900

    feat: 텍스트 컴포넌트 구축

--- talkpick-web/src/App.tsx
+++ talkpick-web/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import "./Shared/DesignSystem/font/font.css";
 import { Heading } from "./Shared/DesignSystem/Heading/Heading";
+import { Text } from "./Shared/DesignSystem/Text/Text";
 function App() {
   return (
     <>
       <Heading variant="heading1">제목 22 ExtraBold </Heading>
       <Heading variant="heading2">부제목 17 SemiBold </Heading>
       <Heading variant="heading3">소제목 15 SemiBold</Heading>
+      <Text>설명 텍스트 12 Medium</Text>
     </>
   );
 }
