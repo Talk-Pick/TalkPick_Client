6d20ded3073c8677a3d5cb3903c94be9ff34a6b2
kwangyoulsagong <sgky0511@naver.com>
2025-05-15 13:39:23 +0900

    feat: 카테고리 카드 버튼 기본적인 Ux 적용

--- talkpick-web/src/Shared/DesignSystem/CategoryCard/CategoryCard.tsx
+++ talkpick-web/src/Shared/DesignSystem/CategoryCard/CategoryCard.tsx
@@ -1,26 +1,35 @@
 import Styled from "@emotion/styled";
 import { CategoryType } from "../Category/Model/Category.type";
-import { css } from "@emotion/react";
 import { Category } from "../Category/Category";
+import { CategoryCardIcons } from "./CategoryCardIcons/CategoryCardIcons";
 
 // styled 컴포넌트 접근법
-const StyleCircle = Styled.circle`
+const StyleButton = Styled.button`
 display:flex;
 flex-direction:column;
 align-items:center;
 justify-content:center;
 width: 158px;
 height: 129px;
+gap:10px;
 border-radius:20px;
 background:#F9F9F9;
+border:none;
+cursor:pointer;
 box-shadow: 0px 6px 7.2px rgba(182, 182, 182, 0.5);
+transition: transform 0.2s ease, box-shadow 0.2s ease;
+ &:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+   
+      }
 `;
 
 export const CategoryCard = ({ category }: { category: CategoryType }) => {
   return (
-    <StyleCircle>
+    <StyleButton>
       <Category category={category} />
-      <span>hello</span>
-    </StyleCircle>
+      <img width={81} height={77} src={CategoryCardIcons[category]} />
+    </StyleButton>
   );
 };
---
+++ talkpick-web/src/Shared/DesignSystem/CategoryCard/CategoryCardIcons/CategoryCardIcons.tsx
@@ -0,0 +1,19 @@
+import date from "../../../../assets/Icons/dateIcon.svg";
+import roommate from "../../../../assets/Icons/roommate.svg";
+import meeting from "../../../../assets/Icons/meeting.svg";
+import friend from "../../../../assets/Icons/friend.svg";
+import family from "../../../../assets/Icons/family.svg";
+import cowoker from "../../../../assets/Icons/cowoker.svg";
+import icebreaking from "../../../../assets/Icons/icebreaking.svg";
+import couple from "../../../../assets/Icons/couple.svg";
+// 아이콘 매핑 객체
+export const CategoryCardIcons = {
+  blind_date: date,
+  roommate_first_meeting: roommate,
+  group_meeting: meeting,
+  friend_meeting: friend,
+  family: family,
+  coworker: cowoker,
+  icebreaking: icebreaking,
+  couple: couple,
+};
